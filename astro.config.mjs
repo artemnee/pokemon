2855b38def493d529669f1f753a95c7eef792eed
artemnee <artemgii1844@gmail.com>
2025-02-20 07:49:56 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import wywInJS from "@wyw-in-js/vite";
+import { defineConfig } from "astro/config"
+import wywInJS from "@wyw-in-js/vite"
 
-import react from '@astrojs/react';
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
-    plugins: [wywInJS()]
-  }
-});
+    plugins: [wywInJS()],
+  },
+  site: "https://artemneepokemon.github.io",
+})
